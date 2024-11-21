ffacc00b98473f2145cd9312ee9f93842f246439
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-21 14:03:09 -0300

    Nova estrutura

--- src/main/java/br/com/totem/service/ComandoService.java
+++ src/main/java/br/com/totem/service/ComandoService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "comando", url = "http://localhost:8080/comando")
+@FeignClient(value = "comando", url = "http://broker-container:8080/comando")
 public interface ComandoService {
 
     @GetMapping("/{mac}")
