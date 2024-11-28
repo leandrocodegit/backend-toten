003d8745cddf557bf073d7b42613c5eceae00df9
leandrocodegit <lpoliveira.ti@gmail.com>
2024-11-28 15:38:52 -0300

    Nova estrutura

--- src/main/java/br/com/totem/security/CorsConfig.java
+++ src/main/java/br/com/totem/security/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Permitir todas as rotas
-                .allowedOrigins("http://vps55601.publiccloud.com.br", "http://broker-container:8082", "http://localhost:4200") // Liste suas origens permitidas
+                .allowedOrigins("http://169.150.1.85","http://vps55601.publiccloud.com.br", "http://broker-container:8082", "http://localhost:4200") // Liste suas origens permitidas
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
