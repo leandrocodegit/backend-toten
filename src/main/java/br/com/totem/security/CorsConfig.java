e117b1b6968514947cc748f8bb3c345dca4878fd
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 02:00:36 -0300

    Configurando ssl

--- src/main/java/br/com/totem/security/CorsConfig.java
+++ src/main/java/br/com/totem/security/CorsConfig.java
@@ -1,18 +1,26 @@
 package br.com.totem.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Permitir todas as rotas
-                .allowedOrigins("http://sincroled.com.br", "http://gateway","http://vps55601.publiccloud.com.br", "http://localhost:4200") // Liste suas origens permitidas
-                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+    @Bean
+    public CorsWebFilter corsConfigurationSource() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin("http://sincroled.com.br");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.addAllowedMethod("*");
+        corsConfig.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
     }
 }
