7296d9f53346d33e86516115f41451282ccafdb1
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 00:53:24 -0300

    Configurando ssl

--- src/main/java/br/com/totem/security/SecurityConfig.java
+++ src/main/java/br/com/totem/security/SecurityConfig.java
@@ -15,6 +15,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
 @Configuration
@@ -44,10 +47,22 @@ public class SecurityConfig {
     };
 
     @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin("http://sincroled.com.br");  // Defina a origem permitida
+        configuration.addAllowedMethod("*");  // Permite todos os métodos HTTP
+        configuration.addAllowedHeader("*");  // Permite todos os cabeçalhos
+        configuration.setAllowCredentials(true);  // Permite credenciais (como cookies ou tokens)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);  // Aplica para todos os endpoints
+        return source;
+    }
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .cors(cors -> cors.disable())
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Habilita CORS com a configuração personalizada
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
