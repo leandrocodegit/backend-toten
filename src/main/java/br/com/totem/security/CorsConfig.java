c642bb535c3d33d87b22237d50d0145c82c6f50b
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-10 02:06:22 -0300

    Configurando ssl

--- src/main/java/br/com/totem/security/CorsConfig.java
+++ src/main/java/br/com/totem/security/CorsConfig.java
@@ -8,7 +8,7 @@ import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
+
 public class CorsConfig {
 
     @Bean
