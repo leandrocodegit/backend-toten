64598783906fef7e5fa69c37bc4286e67195dd7a
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-22 10:15:42 -0300

    Institucional

--- src/main/java/br/com/totem/controller/EmailController.java
+++ src/main/java/br/com/totem/controller/EmailController.java
@@ -3,13 +3,14 @@ package br.com.totem.controller;
 import br.com.totem.controller.request.MensagemEmailRequest;
 import br.com.totem.util.EnviarEmailHTML;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("email")
 public class EmailController {
 
-    @GetMapping
+    @PostMapping
     public void enviarEmail(@RequestBody MensagemEmailRequest request){
         EnviarEmailHTML.enviar(request);
     }
