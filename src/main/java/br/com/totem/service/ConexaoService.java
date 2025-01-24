7794467ce4730b297ea1847080748af205293b11
leandrocodegit <lpoliveira.ti@gmail.com>
2025-01-24 18:40:09 -0300

    Institucional

--- src/main/java/br/com/totem/service/ConexaoService.java
+++ src/main/java/br/com/totem/service/ConexaoService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(value = "comando", url = "${conexao-url}")
+@FeignClient(value = "conexao", url = "${conexao-url}")
 public interface ConexaoService {
 
     @GetMapping
