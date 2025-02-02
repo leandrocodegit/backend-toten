1a962c2fc4af3654bd4470f4b2d6bc54ce113ad8
leandrocodegit <lpoliveira.ti@gmail.com>
2025-02-02 17:44:04 -0300

    Correcao agenda

--- src/main/java/br/com/totem/service/AgendaService.java
+++ src/main/java/br/com/totem/service/AgendaService.java
@@ -114,7 +114,7 @@ public class AgendaService {
 
 
     public void verificaSeAgendaHoje(Agenda agenda){
-        var bool = agenda.getInicio().toLocalDate().equals(LocalDateTime.now().toLocalDate());
+        var bool = agenda.getInicio().toLocalDate().equals(LocalDate.now()) || agenda.getInicio().toLocalDate().isAfter(LocalDate.now());
         if(bool)
             bool = agenda.getTermino().equals(LocalDateTime.now().toLocalDate()) || agenda.getTermino().isBefore(LocalDateTime.now());;
         if(bool){
